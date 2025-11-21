520b6f76d5730cd96245ffc4529d7edb832ee8b7
ub-victor <victoireushindi371@gmail.com>
2025-11-21 16:29:53 +0200

    Initial backend commit

--- streams.js
+++ streams.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blogs3.txt');
 
-readStream.on('data', (chuck)=>{
+readStream.on('data', (chuck) =>{
     console.log('----- NEW CHUNK -----');
     console.log(chuck);
 })
