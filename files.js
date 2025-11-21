459dfa9e9f28b48d8d731a7d9b2f1f4fb3f5a0e7
ub-victor <victoireushindi371@gmail.com>
2025-11-21 13:51:25 +0200

    Initial backend commit

--- files.js
+++ files.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 
 // reading files
 
-fs.readFile('./docs/blogs1.txt', (err, data)=>{
+fs.readFile('./docs/blog1.txt', (err, data)=>{
     if(err){
         console.log(err);
     }
-    console.log(data);
+    //console.log(data); /// it will output a buffer / code the file
+    console.log(data.toString());
 })
 
 // writing files
