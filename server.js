4734cc61a8037f160c0a4f07b2667091420b1e7f
ub-victor <victoireushindi371@gmail.com>
2025-11-22 00:40:55 +0200

    Initial backend commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer((req, res)=>{
         
     }
 
-    fs.readFile('./views/index.html', (err, data)=>{
+    fs.readFile(path, (err, data)=>{
         if(err){
             console.log(err);
             res.end();
