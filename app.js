67851eb194ceb72d60bfeef49bda53f75d016a22
ub-victor <victoireushindi371@gmail.com>
2025-11-22 01:59:16 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/', (req, res)=>{
 
 app.get('/about', (req, res)=>{
     // res.send("<h1>Let us go</h1>")
+    res.sendFile('./views/index.html', {root: __dirname});
 })
 
 app.get('/404', (req, res)=>{
