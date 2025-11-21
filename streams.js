1733ec099c10d6b4dc6c849c5d5e21eaa55d4ff0
ub-victor <victoireushindi371@gmail.com>
2025-11-21 20:51:40 +0200

    Initial backend commit

--- streams.js
+++ streams.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./docs/blog3.txt',{encoding: 'utf8'});
 
 readStream.on('data', (chuck) =>{
-    console.log(date);
     console.log('\n----- NEW CHUNK -----');
     console.log(chuck); // No need to put stringfy as if will get uncode when the fole get in
 });
