33eceb543fb7e81fcef358ec037a3d112d7cadfa
ub-victor <victoireushindi371@gmail.com>
2025-11-22 00:31:48 +0200

    Initial backend commit

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const fs = require('fs');
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method);
     res.setHeader('Content-Tyoe', 'text/html');
+
+    fs.readFile('./views/index.html', (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
     
 
 });
