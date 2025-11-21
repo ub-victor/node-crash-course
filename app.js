9500552b6b0843aefdea9db3a76dccf20a91cd4a
ub-victor <victoireushindi371@gmail.com>
2025-11-22 01:58:10 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.send("<h1>Let us go</h1>")
+})
+
+app.get('/about', (req, res)=>{
+    // res.send("<h1>Let us go</h1>")
+})
+
+app.get('/404', (req, res)=>{
+    res.send("<h1>Let us go</h1>")
+})
+
 
 app.listen(3000)
