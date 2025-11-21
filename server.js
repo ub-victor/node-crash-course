9e69584fbae5d85461e17000c47b1beb31e50b53
ub-victor <victoireushindi371@gmail.com>
2025-11-22 00:54:05 +0200

    Initial backend commit

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ const server = http.createServer((req, res)=>{
             path +=  'about.html';
             res.statusCode = 200;
             break;
-         case '/about':
+         case '/about-me':
             res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
             break;
         default:
             path +=  '404.html';
