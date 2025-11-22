d65790d5c5cda064151d77b362829e7dd77e31ea
ub-victor <victoireushindi371@gmail.com>
2025-11-22 03:19:02 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const express = require('express');
 
+// express app
 const app = express();
 
-// register view engine
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res)=>{
-    res.render('index')
-})
-
-app.get('/about', (req, res)=>{
-    // res.send("<h1>Let us go</h1>")
-    res.sendFile('./views/index.html', {root: __dirname});
-})
-
-app.get('/about', (req, res)=>{
-    res.sendFile('./views/about.html', {root: __dirname});
-})
-
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about')
-})
-// we use use for middleware
-app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
-})
-
+// listen for requests
 app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
