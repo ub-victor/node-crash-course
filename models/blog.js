fe049b0b733c10521b1d24d3ba0459d79fc22582
ub-victor <victoireushindi371@gmail.com>
2025-11-24 18:47:44 +0200

    Initial backend commit

--- models/blog.js
+++ models/blog.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: tue
+    },
+    snippet: {
+        type: String,
+        required: true
+    }
+})
