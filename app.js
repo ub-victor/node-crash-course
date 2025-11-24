e5d36f8a6673a09e0a1e6f94b58076f55cbdaaee
ub-victor <victoireushindi371@gmail.com>
2025-11-24 14:33:19 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
+
 app.use(morgan('dev'));
 
 // register view engine
