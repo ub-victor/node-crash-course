a55755874ee5048cbd9879a5db9c76976511a719
ub-victor <victoireushindi371@gmail.com>
2025-11-24 16:21:49 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 
 //connect to mongodb
 const dbURI = 'mongodb+srv://victoire:victoire@node.65il9u7.mongodb.net/node';
-mongoose.connect(dbURI);
-.then((result)=>console.log("connected to db"))
-.catch((err)=> console.log(err))
+mongoose.connect(dbURI)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 // listen for requests
 app.listen(3000);
