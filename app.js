d2deae29bd72eadc44a85dff05629d64dd2db7eb
ub-victor <victoireushindi371@gmail.com>
2025-11-26 00:16:57 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(morgan('dev'));
 /*
 app.get('/add-blog', (req, res)=>{
   const blog = new Blog({
-    title: 'New song',
-    snippet: "let us  do it",
-    body: 'more about it'
+    title: 'School impact neg/pos',
+    snippet: "How school affects us in both positive and negative ways",
+    body: 'School plays a crucial role in shaping our lives. On the positive side, it provides us with knowledge, social skills, and opportunities for personal growth. It helps us develop critical thinking and problem-solving abilities. Additionally, school fosters friendships and a sense of community. However, there are also negative aspects to consider. The pressure to perform well academically can lead to stress and anxiety. Bullying and peer pressure can negatively impact self-esteem. Furthermore, the rigid structure of traditional education may stifle creativity and individuality. Overall, while school has its drawbacks, the benefits it offers in terms of education and social development are significant.'
   });
   blog.save()
     .then((result)=>{
@@ -56,7 +56,7 @@ app.get('/about', (req, res) => {
 //blog routes
 
 app.get('/blogs', (req, res)=>{ // this route will fetch all the blogs from the database
-  Blog.find() // fetch all the blogs
+  Blog.find().sort({ createdAt: -1 }) // fetch all the blogs from the database and sort them in descending order of creation time
     .then((result)=>{ // result contains all the blogs
       res.render('index', { title: 'All Blogs', blogs: result }) // render the index.ejs file and pass the blogs to it, the render means send the file to the client
     })
