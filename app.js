8025bd28a7faa4c64e3dea3060e328e306ab119e
ub-victor <victoireushindi371@gmail.com>
2025-11-25 19:23:26 +0200

    Initial backend commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
