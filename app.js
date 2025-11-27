59d68f80ce42c3a9e35d1e39ce12d50b40d730bd
ub-victor <victoireushindi371@gmail.com>
2025-11-27 11:32:53 +0200

    get.put,post

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+const morgan = require('morgan'); // it is a middleware for logging requests, it allows us to see the requests made to the server in the terminal
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 
@@ -56,7 +56,10 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' }); // render the about.ejs file , it took views folder by default because of ejs view engine
 });
 
-//blog routes
+// blog routes
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
 app.get('/blogs', (req, res)=>{ // this route will fetch all the blogs from the database
   Blog.find().sort({ createdAt: -1 }) // fetch all the blogs from the database and sort them in descending order of creation time
@@ -68,7 +71,7 @@ app.get('/blogs', (req, res)=>{ // this route will fetch all the blogs from the 
     })
 })
 
-app.post('/blogs', (req, res)=>{
+app.post('/blogs', (req, res)=>{ // this route will handle the form submission to create a new blog
   // console.log(req.body) // req.body contains the form data so the body parser middleware is used to parse the form data which mean the body is the defined in the form 
   const blog = new Blog(req.body); // create a new blog using the form data
   blog.save() // save the blog to the database , the blog here is an instance of the Blog model, by instance we mean an object created from a class and the class here is the Blog model which is created using mongoose.model
@@ -90,6 +93,17 @@ app.get('/blogs/:id', (req, res)=>{
       console.log(err);
     })
 })
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
