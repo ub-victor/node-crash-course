a1e26e63e3acf3b47e7406bc3735041635361665
ub-victor <victoireushindi371@gmail.com>
2025-11-27 12:10:15 +0200

    MVC&Express route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -7,7 +7,7 @@ router.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
-router.get('/blogs', (req, res)=>{ // this route will fetch all the blogs from the database
+router.get('/', (req, res)=>{ // this route will fetch all the blogs from the database
   Blog.find().sort({ createdAt: -1 }) // fetch all the blogs from the database and sort them in descending order of creation time
     .then((result)=>{ // result contains all the blogs
       res.render('index', { title: 'All Blogs', blogs: result }) // render the index.ejs file and pass the blogs to it, the render means send the file to the client
@@ -17,7 +17,7 @@ router.get('/blogs', (req, res)=>{ // this route will fetch all the blogs from t
     })
 })
 
-router.post('/blogs', (req, res)=>{ // this route will handle the form submission to create a new blog
+router.post('/', (req, res)=>{ // this route will handle the form submission to create a new blog
   // console.log(req.body) // req.body contains the form data so the body parser middleware is used to parse the form data which mean the body is the defined in the form 
   const blog = new Blog(req.body); // create a new blog using the form data
   blog.save() // save the blog to the database , the blog here is an instance of the Blog model, by instance we mean an object created from a class and the class here is the Blog model which is created using mongoose.model
@@ -29,7 +29,7 @@ router.post('/blogs', (req, res)=>{ // this route will handle the form submissio
     })
 })
 
-router.get('/blogs/:id', (req, res)=>{
+router.get('/:id', (req, res)=>{
   const id = req.params.id; // get the id from the url
   Blog.findById(id) // find the blog by id
     .then((result)=>{
@@ -40,7 +40,7 @@ router.get('/blogs/:id', (req, res)=>{
     })
 })
 
-router.delete('/blogs/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id;
   Blog.findByIdAndDelete(id)
     .then(result => {
@@ -51,7 +51,7 @@ router.delete('/blogs/:id', (req, res) => {
     });
 });
 
-router.get('/blogs/create', (req, res) => {
+router.get('/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
