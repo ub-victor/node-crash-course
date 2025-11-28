7a5d2eea88ada84bf952cdfa3bdc0926aaa67eeb
ub-victor <victoireushindi371@gmail.com>
2025-11-28 13:39:40 +0200

    express mvc

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -12,12 +12,11 @@ const blog_index = (req, res) => {
 }
 
 
-const blog_details = (req, res)=>{ // this route will handle the form submission to create a new blog
-  // console.log(req.body) // req.body contains the form data so the body parser middleware is used to parse the form data which mean the body is the defined in the form 
-  const blog = new Blog(req.body); // create a new blog using the form data
-  blog.save() // save the blog to the database , the blog here is an instance of the Blog model, by instance we mean an object created from a class and the class here is the Blog model which is created using mongoose.model
+const blog_details = (req, res)=>{
+  const id = req.params.id; // get the id from the url
+  Blog.findById(id) // find the blog by id
     .then((result)=>{
-      res.redirect('/blogs') // redirect to the /blogs route to see the new blog
+      res.render('details', { title: 'Blog Details', blog: result }) // render the details.ejs file and pass the blog to it
     })
     .catch((err)=>{
       console.log(err);
