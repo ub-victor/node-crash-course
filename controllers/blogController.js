12874eb91c170bf52e9e492717781a4e76ef7659
ub-victor <victoireushindi371@gmail.com>
2025-11-28 13:06:29 +0200

    express mvc

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,11 @@
 // blog_index, bloog_details, blog_create_get, blog_create_post, blog_delete
 
 const blog_index = (req, res) => {
-    
+    Blog.find().sort({ createdAt: -1 }) // fetch all the blogs from the database and sort them in descending order of creation time
+    .then((result)=>{ // result contains all the blogs
+      res.render('index', { title: 'All Blogs', blogs: result }) // render the index.ejs file and pass the blogs to it, the render means send the file to the client
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
 }
